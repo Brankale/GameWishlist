656bf010883a220258bfaf111d8121ad06003350
Brankale <45881310+Brankale@users.noreply.github.com>
2019-08-06 15:29:15 +0200

    Update Connectivity.java

--- app/src/main/java/com/fermimn/gamewishlist/utils/Connectivity.java
+++ app/src/main/java/com/fermimn/gamewishlist/utils/Connectivity.java
@@ -2,21 +2,32 @@ package com.fermimn.gamewishlist.utils;
 
 import android.content.Context;
 import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
 import android.net.NetworkInfo;
 
 public class Connectivity {
 
-    private Connectivity() {
-    }
-
-    // TODO: this method should be rewritten to satisfy APIs level 29 security levels
-    // TODO: if you rewrite the method, remove also ACCESS_NETWORK_STATE permission in the manifest
-    // TODO: it doesn't work
+    @SuppressWarnings("deprecation")
     public static boolean isNetworkAvailable(Context context) {
-        ConnectivityManager cm =
-                (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
 
-        // NetworkInfo was deprecated in API level 29 due to security reasons
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // TODO: a revision is needed
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+            Network network = cm.getActiveNetwork();
+
+            if (network == null) {
+                return false;
+            }
+
+            NetworkCapabilities nc = cm.getNetworkCapabilities(network);
+            return nc.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET);
+        }
+
+        // NetworkInfo was deprecated in API level 29
         NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
         return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
