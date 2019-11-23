91add2e140d6aa4c66b9a6cb097185682068554e
Brankale <45881310+Brankale@users.noreply.github.com>
2019-11-23 22:07:57 +0100

    updated recyclerview to v1.1.0
    - code cleanup

--- app/src/main/java/com/fermimn/gamewishlist/App.java
+++ app/src/main/java/com/fermimn/gamewishlist/App.java
@@ -12,12 +12,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
-import android.graphics.RectF;
 import android.os.Build;
 import android.util.Log;
 
