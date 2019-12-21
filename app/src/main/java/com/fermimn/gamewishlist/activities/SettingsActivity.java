87db96c2e53b42844d93ca7b6c020f6920b4ab2f
Brankale <45881310+Brankale@users.noreply.github.com>
2019-12-21 23:34:54 +0100

    added toolbar in settings

--- app/src/main/java/com/fermimn/gamewishlist/activities/SettingsActivity.java
+++ app/src/main/java/com/fermimn/gamewishlist/activities/SettingsActivity.java
@@ -4,18 +4,25 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import com.fermimn.gamewishlist.R;
 
 public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar toolbar = findViewById(R.id.action_bar);
+        toolbar.setTitle( getString(R.string.section_settings) );
+        setSupportActionBar(toolbar);
 
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(android.R.id.content, new SettingsFragment())
+                .add(R.id.container, new SettingsFragment())
                 .commit();
-
     }
 
 }
