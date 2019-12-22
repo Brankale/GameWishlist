c65ea9fbdd4691609496a163775ba976c9477b49
Brankale <45881310+Brankale@users.noreply.github.com>
2019-12-22 01:00:05 +0100

    removed kotlin classes and libraries

---
+++ app/src/main/java/com/fermimn/gamewishlist/activities/SettingsFragment.java
@@ -0,0 +1,14 @@
+package com.fermimn.gamewishlist.activities;
+
+import android.os.Bundle;
+import androidx.preference.PreferenceFragmentCompat;
+import com.fermimn.gamewishlist.R;
+
+public class SettingsFragment extends PreferenceFragmentCompat {
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        addPreferencesFromResource(R.xml.preferences);
+    }
+
+}
